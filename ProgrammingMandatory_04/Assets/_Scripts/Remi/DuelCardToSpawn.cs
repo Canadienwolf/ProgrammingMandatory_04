42c833e8e59c86fb33f224e4dd8d53dedcba85c7
FroztySeven <froztyseven@hotmail.com>
2021-04-19 00:40:45 +0200

    Remi scene progress

--- ProgrammingMandatory_04/Assets/_Scripts/Remi/DuelCardToSpawn.cs
+++ ProgrammingMandatory_04/Assets/_Scripts/Remi/DuelCardToSpawn.cs
@@ -14,23 +14,37 @@ public class DuelCardToSpawn : MonoBehaviour
     void Start()
     {
         dealerDeck.SetActive(false);
-        var cardCoverInstance = Instantiate(CardCover, transform.position, transform.rotation);
-        cardCoverInstance.transform.SetParent(transform, false);
+        // var cardCoverInstance = Instantiate(CardCover, transform.position, transform.rotation);
+        // cardCoverInstance.transform.SetParent(transform, false);
     }
 
     public void RevealCardCollection()
     {
-        var cardActualInstance = Instantiate (Cards[Random.Range(0,Cards.Length)], transform.position, transform.rotation);
-        cardActualInstance.transform.SetParent(transform, false);
+        var cardRevealInstance = Instantiate (Cards[Random.Range(0,Cards.Length)], transform.position, transform.rotation);
+        cardRevealInstance.transform.SetParent(transform, false);
         
-        var CardActualInstanceDealer = Instantiate(cardActualInstance, new Vector3(0, -350, 0), transform.rotation);
-        CardActualInstanceDealer.transform.SetParent(dealerDeck.transform, false);
-        CardActualInstanceDealer.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        var cardRevealDealerInstance = Instantiate(cardRevealInstance, new Vector3(0, -350, 0), transform.rotation);
+        cardRevealDealerInstance.transform.SetParent(dealerDeck.transform, false);
+        cardRevealDealerInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        cardRevealDealerInstance.GetComponent<Button>().onClick.AddListener(this.ButtonClicked);
+    }
+    public void ButtonClicked() 
+    {
+        PlayerPrefs.SetString("PrefsDealersChoice", transform.GetChild(0).name.Replace("(Clone)", ""));
+        print("Dealer has chosen card: " + PlayerPrefs.GetString("PrefsDealersChoice"));
+        
+        GameObject.Find("DealerDeck").SetActive(false);
+        GameObject.Find("Text_GameStatus").GetComponent<Text>().text =
+            transform.GetChild(0).name.Replace("(Clone)", "") + " was chosen!. \nP1 or P2, Quick find the blue card!";
     }
 
     public void HideCardCollection()
     {
-        transform.GetChild(1).GetComponent<Image>().sprite = Resources.Load<Sprite>("BackColor_Blue");
+        // transform.GetChild(1).GetComponent<Image>().sprite = Resources.Load<Sprite>("BackColor_Blue");
+        
+        var cardCoverInstance = Instantiate(CardCover, transform.position, transform.rotation);
+        cardCoverInstance.transform.SetParent(transform, false);
+        
         dealerDeck.SetActive(true);
     }
 
--- ProgrammingMandatory_04/Assets/_Scripts/Remi/DuelGameController.cs
+++ ProgrammingMandatory_04/Assets/_Scripts/Remi/DuelGameController.cs
@@ -3,17 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class DuelGameController : MonoBehaviour
 {
     public Text statusText;
 
-    public float memorizeTime = 10f;
+    public float memorizeTime = 5f;
     public bool canDoTimer;
 
     private void Start()
     {
         statusText.text = "";
+    }
+    
+    public string[] GetDuelerChildrenNames(Transform trans)
+    {
+        List<string> _returnValue = new List<string>();
+
+        foreach (Transform item in trans)
+        {
+            _returnValue.Add(item.gameObject.name);
+        }
+
+        return _returnValue.ToArray();
     }
 
     public void _StartCountdown()
@@ -47,8 +60,17 @@ public class DuelGameController : MonoBehaviour
                 transform.Find("Placeholder (4)").GetComponent<DuelCardToSpawn>().HideCardCollection();
                 transform.Find("Placeholder (5)").GetComponent<DuelCardToSpawn>().HideCardCollection();
                 
-                statusText.text = "Dealer, pick a card from the collection!";
+                statusText.text = "Dealer is choosing a card...";
+                
+                int childIndex = Random.Range(0, GameObject.Find("DealerDeck").gameObject.transform.childCount);
+                
+                print(childIndex);
+
+                // var randomDealIndex = Random.Range(0, GameObject.Find("DealerDeck").gameObject.transform.name.childCount);
+                // randomDealIndex =
+                //     PlayerPrefs.SetString("PrefsDealersChoice", transform.GetChild(0).name.Replace("(Clone)", ""));
             }
         }
+        
     }
 }
