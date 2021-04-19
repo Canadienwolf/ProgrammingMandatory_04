0f7f61e38c7911f4da6c0d9e02336ef8616e088c
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2021-04-19 21:30:27 +0200

    winning behaviour changes

--- ProgrammingMandatory_04/Assets/Scenes/Ayoub/RandomMovementsScript.cs
+++ ProgrammingMandatory_04/Assets/Scenes/Ayoub/RandomMovementsScript.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomMovementsScript : MonoBehaviour
 {
     public GameObject[] cards;
+
+    public GameObject winningText;
+
+    private string[] winString = { "Spade", "Heart", "Diamond", "Clover" };
     // Start is called before the first frame update
     void Start()
     {
+        winningText.SetActive(false);
         cards = GameObject.FindGameObjectsWithTag("Cards2");
         //MoveCards(); call it to move the cards!
     }
@@ -16,6 +22,12 @@ public class RandomMovementsScript : MonoBehaviour
     public void MoveCards()
     {
         int i = Random.RandomRange(0,4);
-        cards[i].transform.position = new Vector3(cards[i].transform.position.x -0.5f, cards[i].transform.position.y, cards[i].transform.position.z);
+        cards[i].transform.position = new Vector3(cards[i].transform.position.x -0.45f, cards[i].transform.position.y, cards[i].transform.position.z);
+        if (cards[i].transform.position.x <=-1.3f )
+        {
+            winningText.GetComponent<Text>().text = winString[i].ToString();
+            winningText.SetActive(true);
+            
+        }
     }
 }
